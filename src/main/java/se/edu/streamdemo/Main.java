bef21e95744f7971340a6e067c30c5cf7a2fdff6
E0958902 <E0958902@u.nus.edu>
2024-02-25 22:50:52 +0800

    Implement print all data using streams

--- src/main/java/se/edu/streamdemo/Main.java
+++ src/main/java/se/edu/streamdemo/Main.java
@@ -43,6 +43,11 @@ public class Main {
         }
     }
 
+    public static void printAllDataUsingStream(ArrayList<Task> tasks) {
+        System.out.println("Printing data using stream ...");
+        tasks.stream()
+                .forEach(System.out::println);
+    }
     public static void printDeadlines(ArrayList<Task> tasksData) {
         for (Task t : tasksData) {
             if (t instanceof Deadline) {
